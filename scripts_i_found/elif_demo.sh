451d546a4b67d87853b1671d6cc23183a0c084b6
Elijah Hawk <elijahmhawk@gmail.com>
2023-01-22 16:17:13 -0800

    using single bracket instead of double

--- scripts_i_found/elif_demo.sh
+++ scripts_i_found/elif_demo.sh
@@ -3,10 +3,10 @@
 echo -n "Enter a number: "
 read VAR
 
-if [[ $VAR -gt 10 ]]
+if [ $VAR -gt 10 ]
 then
   echo "The variable is greater than 10."
-elif [[ $VAR -eq 10 ]]
+elif [ $VAR -eq 10 ]
 then
   echo "The variable is equal to 10."
 else
